309431df11baca9aa110205acb57211513e269db
Thanaphas <5706021632049@fitm.kmutnb.ac.th>
2016-12-11 11:54:48 +0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,124 +1,117 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+'use strict'
 
-app.use(bodyParser.json())
-app.set('port', (process.env.PORT || 4000))
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const axios = require('axios')
+const app = express()
+const token = 'EAAQ7MwzNNKUBAE108aJeAbcTBUj8Kifhx9qiU0HTTtBPRLS6dZCuF7PuTFtfahL6SpgvQcHboCZAYPJmHWrnBVn40qkv810hTJoras0tN2RhqwJ3ryu8GHtZBiTYZCBNjdtU626qQWZBu5aE2ZC2QvE3UWvLmUhVFAQySDFkGI0gZDZD'
+app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
-app.get('/webhook', function(req, res) {
-  var key = 'EAAQ7MwzNNKUBAC5oGlLjeZC3WKXvZCfZCpeBMchtxzFl0Qw3oCxAL1PGZCbKYsECVeXKramCdvtFNctkzCely6y7AQoD7yNwNlEMxSZBLMbeZC3SvknUjO12BGi0pVBtHSG0Xw2JzSpZCxqMY6n6hARFRg4hVuDtTqYPw8vE7OdtwZDZD'
-  if (req.query['hub.verify_token'] === key) {
+app.get('/', function (req, res) {
+  res.send('test test')
+})
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'weather') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
 })
-
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know
-    // you've successfully received the callback. Otherwise, the request
-    // will time out and we will keep trying to resend.
-    res.sendStatus(200);
+app.post('/webhook/', function (req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      if (text === 'Generic') {
+        sendGenericMessage(sender)
+        continue
+      }
+      // sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
+        axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + text + '&APPID=7fee5476cbd1705fb181c28e20c473b7').then(function (res) {
+          console.log(res.data.main.temp)
+          sendTextMessage(sender, res.data.main.temp - 273)
+        })
+    }
+    if (event.postback) {
+      let text = JSON.stringify(event.postback)
+      sendTextMessage(sender, 'Postback received: ' + text.substring(0, 200), token)
+      continue
+    }
   }
-});
+  res.sendStatus(200)
+})
 
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
-
-  console.log("Received message for user %d and page %d at %d with message:",
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
-
-  var messageId = message.mid;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-
-  if (messageText) {
-    if (messageText === 'hello') {
-      sendTextMessage(senderID, "หวัดดี");
-    }
-
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
-}
-function sendGenericMessage(recipientId, messageText) {
-  // To be expanded in later sections
-}
-
-function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
+function sendTextMessage (sender, text) {
+  let messageData = { text: text }
   request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAAQ7MwzNNKUBAC5oGlLjeZC3WKXvZCfZCpeBMchtxzFl0Qw3oCxAL1PGZCbKYsECVeXKramCdvtFNctkzCely6y7AQoD7yNwNlEMxSZBLMbeZC3SvknUjO12BGi0pVBtHSG0Xw2JzSpZCxqMY6n6hARFRg4hVuDtTqYPw8vE7OdtwZDZD' },
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
     method: 'POST',
-    json: messageData
-
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
-
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
+    json: {
+      recipient: {id: sender},
+      message: messageData
     }
-  });
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
+function sendGenericMessage (sender) {
+  let messageData = {
+    'attachment': {
+      'type': 'template',
+      'payload': {
+        'template_type': 'generic',
+        'elements': [{
+          'title': 'First card',
+          'subtitle': 'Element #1 of an hscroll',
+          'image_url': 'http://messengerdemo.parseapp.com/img/rift.png',
+          'buttons': [{
+            'type': 'web_url',
+            'url': 'https://www.messenger.com',
+            'title': 'web url'
+          }, {
+            'type': 'postback',
+            'title': 'Postback',
+            'payload': 'Payload for first element in a generic bubble'
+          }]
+        }, {
+          'title': 'Second card',
+          'subtitle': 'Element #2 of an hscroll',
+          'image_url': 'http://messengerdemo.parseapp.com/img/gearvr.png',
+          'buttons': [{
+            'type': 'postback',
+            'title': 'Postback',
+            'payload': 'Payload for second element in a generic bubble'
+          }]
+        }]
+      }
+    }
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
 }
 
 app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
+  console.log('running on port', app.get('port'))
 })
